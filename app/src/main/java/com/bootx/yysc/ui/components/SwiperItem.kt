d3a748413487c9d042feed9807ff13189db20093
1169794338@qq.com <1169794338@qq.com>
2023-11-18 00:35:56 +0800

    init

---
+++ app/src/main/java/com/bootx/yysc/ui/components/SwiperItem.kt
@@ -0,0 +1,116 @@
+package com.bootx.yysc.ui.components
+
+import android.util.Log
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.util.lerp
+import coil.compose.AsyncImage
+import com.bootx.yysc.ui.theme.fontSize10
+import kotlin.math.absoluteValue
+
+data class SwiperItemEntity(
+    val logo: String,
+    val image: String,
+    val title1: String = "",
+    val title2: String = "",
+    val btn: String = "",
+)
+
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+fun SwiperItem(items: List<SwiperItemEntity>) {
+    val pagerState = rememberPagerState(pageCount = {
+        items.size
+    })
+    HorizontalPager(
+        state = pagerState,
+        verticalAlignment = Alignment.Top,
+    ) { page ->
+        Box(
+
+        ) {
+            AsyncImage(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp)
+                    .clip(RoundedCornerShape(8.dp))
+                    .background(Color.DarkGray)
+                    .height(160.dp)
+                    .graphicsLayer {
+                        val pageOffset =
+                            ((pagerState.currentPage - page) + pagerState.currentPageOffsetFraction).absoluteValue
+                        Log.e("HomeScreen", pagerState.currentPage.toString())
+                        alpha = lerp(
+                            start = 0.5f,
+                            stop = 1f,
+                            fraction = 1f - pageOffset.coerceIn(0f, 1f)
+                        )
+                    },
+                model = items[page].image,
+                contentDescription = ""
+            )
+            Box(
+                Modifier
+                    .align(Alignment.BottomStart)
+                    .padding(start = 24.dp, bottom = 24.dp)
+            ) {
+                AsyncImage(
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier
+                        .size(40.dp)
+                        .clip(RoundedCornerShape(8.dp))
+                        .background(Color.LightGray),
+                    model = items[page].logo,
+                    contentDescription = ""
+                )
+            }
+            Box(
+                Modifier
+                    .align(Alignment.BottomStart)
+                    .padding(start = 80.dp, bottom = 40.dp)
+            ) {
+                Text(text = items[page].title1, color = Color.White, fontSize = fontSize10)
+            }
+
+            Box(
+                Modifier
+                    .align(Alignment.BottomStart)
+                    .padding(start = 80.dp, bottom = 24.dp)
+            ) {
+                Text(text = items[page].title2, color = Color.White, fontSize = fontSize10)
+            }
+            Box(
+                Modifier
+                    .align(Alignment.BottomEnd)
+                    .padding(end = 24.dp, bottom = 24.dp)
+            ) {
+                Button(
+                    modifier = Modifier
+                        .height(32.dp)
+                        .align(Alignment.Center),
+                    onClick = { /*TODO*/ },
+                ){
+                    Text(text = "下载")
+                }
+            }
+        }
+    }
+}
--- app/src/main/java/com/bootx/yysc/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/bootx/yysc/ui/screens/HomeScreen.kt
@@ -1,17 +1,12 @@
 package com.bootx.yysc.ui.screens
 
-import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -19,30 +14,20 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.foundation.pager.HorizontalPager
-import androidx.compose.foundation.pager.PagerState
-import androidx.compose.foundation.pager.rememberPagerState
-import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowForwardIos
 import androidx.compose.material.icons.filled.Star
 import androidx.compose.material3.Button
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardColors
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
@@ -55,6 +40,8 @@ import com.bootx.yysc.model.entity.SoftEntity
 import com.bootx.yysc.ui.components.AdData
 import com.bootx.yysc.ui.components.MyCard
 import com.bootx.yysc.ui.components.SoftItem
+import com.bootx.yysc.ui.components.SwiperItem
+import com.bootx.yysc.ui.components.SwiperItemEntity
 import com.bootx.yysc.ui.theme.backgroundColor
 import com.bootx.yysc.ui.theme.fontSize12
 import com.bootx.yysc.ui.theme.height16
@@ -78,47 +65,24 @@ var itemList = listOf<ItemList>(
     ItemList(R.drawable.qiandao, "签到"),
 )
 
-@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun HomeScreen(navController: NavHostController) {
-    val pagerState = rememberPagerState { 10 }
-    val toolbarHeight = 48.dp
-    val toolbarHeightPx = with(LocalDensity.current) { toolbarHeight.roundToPx().toFloat() }
-    val toolbarOffsetHeightPx = remember { mutableStateOf(0f) }
+    var items = listOf(
+        SwiperItemEntity(
+            title1 = "title1",
+            title2 = "title2",
+            logo = "logo",
+            image = "image",
+        )
+    )
     Surface(
         modifier = Modifier.fillMaxHeight(),
         color = backgroundColor,
     ) {
-        val paddingOffset =
-            toolbarHeight + with(LocalDensity.current) { toolbarOffsetHeightPx.value.toDp() }
-        HorizontalPager(
-            modifier = Modifier.fillMaxSize(),
-            state = pagerState,
-            contentPadding = PaddingValues(top = paddingOffset)
-        ) {
-            Column(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .verticalScroll(rememberScrollState())
-            ) {
-                repeat(20) {
-                    Box(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .height(64.dp)
-                            .padding(4.dp)
-                            .background(if (it % 2 == 0) Color.Black else Color.Yellow),
-                        contentAlignment = Alignment.Center
-                    ) {
-                        Text(
-                            text = it.toString(),
-                            color = if (it % 2 != 0) Color.Black else Color.Yellow
-                        )
-                    }
-                }
+        LazyColumn(){
+            item {
+                SwiperItem(items)
             }
-        }
-        LazyColumn() {
             item {
                 Row(
                     modifier = Modifier
@@ -235,62 +199,6 @@ fun HomeScreen(navController: NavHostController) {
                 AdData()
             }
             item {
-                MyCard("好评如潮") {
-                    LazyRow {
-                        items(100) {
-                            Column(
-                                modifier = Modifier.width(100.dp),
-                                horizontalAlignment = Alignment.CenterHorizontally,
-                                verticalArrangement = Arrangement.Center,
-                            ) {
-                                Spacer(modifier = Modifier.height(height8))
-                                AsyncImage(
-                                    modifier = Modifier.size(80.dp),
-                                    model = "https://pp.myapp.com/ma_icon/0/icon_93301_1699860270/256",
-                                    contentDescription = ""
-                                )
-                                Spacer(modifier = Modifier.height(height8))
-                                Text(
-                                    text = "WiFi万能钥匙",
-                                    maxLines = 1,
-                                    overflow = TextOverflow.Ellipsis,
-                                )
-                                Spacer(modifier = Modifier.height(height8))
-                                Row(
-                                    modifier = Modifier.fillMaxWidth(),
-                                    horizontalArrangement = Arrangement.Center,
-                                    verticalAlignment = Alignment.CenterVertically,
-                                ) {
-                                    Icon(
-                                        imageVector = Icons.Filled.Star,
-                                        contentDescription = "",
-                                        tint = MaterialTheme.colorScheme.primary
-                                    )
-                                    Text(
-                                        text = "9.9",
-                                        maxLines = 1,
-                                        overflow = TextOverflow.Ellipsis,
-                                        fontWeight = FontWeight.Bold,
-                                        fontSize = 12.sp,
-                                        color = MaterialTheme.colorScheme.primary,
-                                    )
-                                }
-                                Spacer(modifier = Modifier.height(height8))
-                                Button(
-                                    modifier = Modifier
-                                        .fillMaxWidth(0.9f)
-                                        .height(height32),
-                                    onClick = { /*TODO*/ },
-                                ) {
-                                    Text(text = "下载", fontSize = fontSize12, color = Color.White)
-                                }
-                                Spacer(modifier = Modifier.height(height8))
-                            }
-                        }
-                    }
-                }
-            }
-            item {
                 MyCard(title = "热门下载") {
                     Column {
                         val soft = SoftEntity(
@@ -338,9 +246,10 @@ fun HomeScreen(navController: NavHostController) {
                     }
                 }
             }
-            item{
-                Spacer(modifier = Modifier.height(64.dp))
+            item {
+                Spacer(modifier = Modifier.height(32.dp))
             }
         }
     }
+
 }
