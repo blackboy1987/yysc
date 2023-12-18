6796ebcf3c30b12bd077a0020f3a8c134cbf3bcf
xiali <1169794338@qq.com>
2023-12-18 10:21:13 +0800

    init

--- app/src/main/java/com/bootx/yysc/config/Config.kt
+++ app/src/main/java/com/bootx/yysc/config/Config.kt
@@ -2,9 +2,9 @@ package com.bootx.yysc.config
 
 object Config {
 
-    // var baseUrl = "http://yysc.igomall.xin"
+    var baseUrl = "http://yysc.igomall.xin"
     //var baseUrl = "https://965z2991e.oicp.vip"
-    var baseUrl = "http://192.168.31.214:9901"
+    // var baseUrl = "http://192.168.31.214:9901"
     //var baseUrl = "http://172.16.12.97:9901"
     var messageTypeList = listOf("系统消息", "收到的点赞", "应用状态", "收到的投币", "好友关注")
     var imageUrl = "https://bootx-tuchuang.oss-cn-hangzhou.aliyuncs.com/"
