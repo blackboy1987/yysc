cc4a5cf3745484ffc522fff5541c01eea641a1a7
xiali <1169794338@qq.com>
2023-12-20 18:11:22 +0800

    init

--- app/src/main/java/com/bootx/yysc/ui/screens/SearchScreen.kt
+++ app/src/main/java/com/bootx/yysc/ui/screens/SearchScreen.kt
@@ -1,6 +1,5 @@
 package com.bootx.yysc.ui.screens
 
-import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
@@ -44,14 +43,12 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.buildAnnotatedString
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
@@ -61,7 +58,6 @@ import coil.compose.AsyncImage
 import com.bootx.yysc.model.entity.SoftEntity
 import com.bootx.yysc.ui.components.LeftIcon
 import com.bootx.yysc.ui.components.SoftItem
-import com.bootx.yysc.ui.components.TabRowList
 import com.bootx.yysc.ui.components.TabRowList
 import com.bootx.yysc.ui.theme.fontSize12
 import com.bootx.yysc.util.StoreManager
@@ -91,7 +87,6 @@ fun SearchScreen(
         hotList = softViewModel.orderBy(context, 1, 20, "8")
         // 热门搜索
         hotSearchViewModel.fetchList(context)
-        Log.e("LaunchedEffect", "SearchScreen: ${hotSearchViewModel.list.size}")
     }
     val gson = Gson()
     var keywords by remember {
@@ -211,9 +206,6 @@ fun SearchScreen(
                 })
                 LazyColumn(
                     modifier = Modifier.padding(horizontal = 16.dp),
-                ) {
-
-                    modifier = Modifier.padding(start = 16.dp, end = 16.dp, top = 64.dp),
                 ) {
                     if (selectedTabIndex == 0) {
                         items(200) {
